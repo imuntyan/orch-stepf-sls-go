d8123ea78b107c0b95b3ace467462046b3d7d572
Igor Muntyan <igor@setsail.co>
2022-05-11 14:51:35 -0400

    working successful ECS task

--- ecs/go/worker/main.go
+++ ecs/go/worker/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/pkg/errors"
 	"log"
+	"os"
 )
 
 func main() {
-	err := errors.New("Work In Progress")
-	log.Fatal(err)
+	failStagesEnv, ok := os.LookupEnv("failStages")
+	if !ok {
+		log.Fatal(errors.New("no failStages env var"))
+	}
+	stageEnv, ok := os.LookupEnv("stage")
+	if !ok {
+		log.Fatal(errors.New("no stage env var"))
+	}
+
+	failStages := make([]string, 0)
+	if err := json.Unmarshal([]byte(failStagesEnv), &failStages); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fs := range failStages {
+		if fs == stageEnv {
+			err := errors.Errorf("Simulated failure of the stage %s", stageEnv)
+			log.Fatal(err)
+		}
+	}
+
+	log.Printf("Stage %s completed successfully", stageEnv)
 }
